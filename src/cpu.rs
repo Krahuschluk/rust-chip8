a29587e1abd79b6acf1316b8280ca46e2d0e91dc
Krahuschluk <fehuschluk@gmail.com>
2019-08-23 18:05:14 +0900

    Added CPU variables and chaotic comments

--- src/cpu.rs
+++ src/cpu.rs
@@ -5,8 +5,18 @@ fn add(a: i32, b: i32) -> i32{
     a + b
 }
 
+struct CPU {
+    opcode: u16,
+    memory: [u8; 4096],
+    cpu_register: [u8; 16],
 
-fn merge_opcodes(first: u8, second: u8) -> u16{
+    index: u16,
+    program_counter: u16,
+    stack: [u16; 16],
+    stack_pointer: u16,
+}
+
+fn merge_opcodes(first: u8, second: u8) -> u16 {
     let f = first as u16;
     let s = second as u16;
     let r = (f << 8) | s;
@@ -16,6 +26,34 @@ fn merge_opcodes(first: u8, second: u8) -> u16{
     r
 }
 
+fn main() {
+    let opcode: u16;
+    let memory: [u8; 4096];
+    let cpu_register: [u8; 16];
+    let index: u16;
+    let program_counter: u16;
+
+    // Stack stores the program counter when calling a subroutine
+    let stack: [u16; 16];
+    let stack_pointer: u16;
+}
+
+fn emulate_cycle() {
+    // Fetch opcode
+    // pc will point to memory, where to pick up opcode
+    
+    // Decode opcode
+    // Execute opcode
+    //
+    // Update timers
+}
+
+fn fetch_opcode() {
+    // Inputs: pc, memory
+    // Output: opcode
+    //
+    // Use pc and pc + 1, merge
+}
 
 #[cfg(test)]
 mod tests {
