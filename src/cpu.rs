f351b12986b31af031a1cb7f4ac6a60861b7bfe3
Krah <fehuschluk@gmail.com>
2019-08-24 23:37:24 +0900

    Messing around in CPU trying to figure out syntax

--- src/cpu.rs
+++ src/cpu.rs
@@ -5,15 +5,40 @@ fn add(a: i32, b: i32) -> i32{
     a + b
 }
 
-struct CPU {
-    opcode: u16,
-    memory: [u8; 4096],
-    cpu_register: [u8; 16],
+//struct CPU {
+//    opcode: u16,
+//
+//    cpu_register: [u8; 16],
+//
+//    index: u16,
+//
+//
+//}
+//
+//struct Memory {
+//    memory: [u8; 4096],
+//    program_counter: u16,
+//}
+//
+//impl Memory {
+//    fn store(&mut self, v: u8) {
+//        self.memory[1] = v;
+//    }
+//
+//    fn fetch(&mut self, val: usize) {
+//        self.memory[val];
+//    }
+//
+//    fn counter_up(&mut self) {
+//        self.program_counter += 1;
+//    }
+//}
 
-    index: u16,
-    program_counter: u16,
+
+struct Stack {
     stack: [u16; 16],
-    stack_pointer: u16,
+    pointer: u16,
+
 }
 
 fn merge_opcodes(first: u8, second: u8) -> u16 {
@@ -38,22 +63,43 @@ fn main() {
     let stack_pointer: u16;
 }
 
-fn emulate_cycle() {
-    // Fetch opcode
-    // pc will point to memory, where to pick up opcode
-    
-    // Decode opcode
-    // Execute opcode
-    //
-    // Update timers
+pub struct CPU {
+    // Maybe inject these from the outside when initializing?
+    pub memory: [u8; 4096],
+    pub program_counter: u16,
+    pub stack: [u16; 16],
+    pub stack_pointer: u16,
+    pub cpu_register: [u8; 16],
+    pub opcode: u16,
+    pub index: u16,
 }
 
-fn fetch_opcode() {
-    // Inputs: pc, memory
-    // Output: opcode
-    //
-    // Use pc and pc + 1, merge
+
+
+impl CPU {
+    pub fn emulate_cycle(&self) {
+        // Fetch opcode
+        // pc will point to memory, where to pick up opcode
+        let opcode = CPU::fetch_opcode(self.program_counter, self.memory);
+
+        // Decode opcode
+        // Execute opcode
+        //
+        // Update timers
+    }
+
+    fn fetch_opcode(pc: u16, mem: [u8; 4096]) {
+        // Inputs: pc, memory
+        // Output: opcode
+        //
+        // Use pc and pc + 1, merge
+        let counter = pc as usize;
+
+        merge_opcodes(mem[counter], mem[counter + 1]);
+    }
 }
+
+
 
 #[cfg(test)]
 mod tests {
--- src/main.rs
+++ src/main.rs
@@ -2,4 +2,13 @@ mod cpu;
 
 fn main() {
     println!("Hello, world!");
+    let cpu = cpu::CPU {
+        memory: [123; 4096],
+        program_counter: 0,
+        stack: [0; 16],
+        stack_pointer: 0,
+        cpu_register: [0; 16],
+        opcode: 0,
+        index: 0,
+    };
 }
